b5ce84b4cc84cadaed3ea2c14ee431b8793bb04e
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-29 14:56:04 -0500

    implemented continious integration with travis ci

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '361818254568-qjmcjkg8j159a3su057cv3cnhd7o9cto.apps.googleusercontent.com',
+  googleClientSecret: 'FKWwGQ3IDg3yd1yt_GCeGVca',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  redisURI: 'redis://127.0.0.1:6379',
+  cookieKey: '123123123'
+}
