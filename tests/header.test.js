1181be011726b202f8ef32c2f2f6184af886aa71
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-29 10:01:04 -0500

    refactored tests to make use of factory functions;

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer')
-let browser, page
+const sessionFactory = require('./factories/sessionFactory')
+const userFactory = require('./factories/userFactory')
 
+let browser, page
 beforeEach(async () => {
   browser = await puppeteer.launch({
     headless: false
@@ -25,20 +27,10 @@ test('clicking login starts oauth flow', async () => {
 })
 
 test('when signed in, show logout button', async () => {
-  const id = '5cb601da482d428d341d4358'
-  const Buffer = require('safe-buffer').Buffer
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  }
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
-  const Keygrip = require('keygrip')
-  const keys = require('../config/keys')
-  const keygrip = new Keygrip([keys.cookieKey])
-  const sig = keygrip.sign('session=' + sessionString)
-
-  await page.setCookie({ name: 'session', value: sessionString })
+  const user = await userFactory()
+  const { session, sig } = sessionFactory(user)
+  
+  await page.setCookie({ name: 'session', value: session })
   await page.setCookie({ name: 'session.sig', value: sig })
   await page.goto('localhost:3000')
   await page.waitFor('a[href="/auth/logout"]')
