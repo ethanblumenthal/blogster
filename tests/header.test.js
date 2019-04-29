83347d740aafc9358d5f5e7ba29972fdafc8a1df
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-29 09:25:59 -0500

    created test for logout flow

--- tests/header.test.js
+++ tests/header.test.js
@@ -37,7 +37,12 @@ test('when signed in, show logout button', async () => {
   const keys = require('../config/keys')
   const keygrip = new Keygrip([keys.cookieKey])
   const sig = keygrip.sign('session=' + sessionString)
+
   await page.setCookie({ name: 'session', value: sessionString })
   await page.setCookie({ name: 'session.sig', value: sig })
   await page.goto('localhost:3000')
+  await page.waitFor('a[href="/auth/logout"]')
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+  expect(text).toEqual('Logout')
 })
